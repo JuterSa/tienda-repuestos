6ac8f1a06d659d5cbc89cf006a5a25fd35809e46
Saul Julio Teran <sSj>
2025-01-21 09:28:27 -0500

    Subiendo actualizacion

--- todo-repuesto-back/src/main/java/com/TodoRepuesto/todorepuesto/TodoRepuestoApplication.java
+++ todo-repuesto-back/src/main/java/com/TodoRepuesto/todorepuesto/TodoRepuestoApplication.java
@@ -8,7 +8,7 @@ public class TodoRepuestoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoRepuestoApplication.class, args);
-		System.out.println("Tu aplicactivo se esta ejecutando sin ningun problema");
+		System.out.println("Zona de respuestos coneccted");
 	}
 
 }
